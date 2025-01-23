448cc47b4cc3c7baf9397dde4b0c40a124d64b19
MykaelLeal <mikaelleal05@gmail.com>
2025-01-23 14:52:09 -0300

    quinto commit

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -3,8 +3,10 @@ import { useState } from 'react';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
 import Search from './components/Search';
+import Filter from './components/Filter';
 
-import './App.css'
+import './App.css';
+
 
 function App() {
 
@@ -34,6 +36,9 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
 
@@ -70,11 +75,23 @@ function App() {
     <div className="container">
     <h2>Lista de Tarefas</h2>
     <Search search={search} setSearch={setSearch} />
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
     <div className="todo-list">
       {todos
+        .filter((todo) => 
+          filter === 'All' 
+            ? true 
+            : filter === 'Completed' 
+            ? todo.isCompleted 
+            : !todo.isCompleted
+          )
         .filter((todo) =>
           todo.text.toLowerCase().includes(search.toLowerCase()) 
         )
+        .sort((a, b) => sort === 'Asc' 
+           ? a.text.localeCompare(b.text) 
+           : b.text.localeCompare(a.text)
+          )
         .map((todo) => (
           <Todo
             key={todo.id}
